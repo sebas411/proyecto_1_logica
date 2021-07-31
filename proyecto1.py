e05eb63629c8b703c1e11bb6ff1078e4075b4d6f
sebas411 <59925024+sebas411@users.noreply.github.com>
2021-07-31 10:50:42 -0600

    Add files via upload

--- proyecto1.py
+++ proyecto1.py
@@ -1,4 +1,6 @@
 import itertools as it
+import random
+import copy as cp
 
 def bruteForce(clause):
   variables = []
@@ -28,27 +30,86 @@ def bruteForce(clause):
       return (True, posibility)
   return (False, None)
 
+def selectVariable(clause):
+  variables = []
+
+  for item in clause:
+    for var in item:
+      if var[0] == "!": var = var[1]
+      if not var in variables: variables.append(var)
+
+  return variables[random.randint(0, len(variables)-1)]
+  
         
 
 def DPLL(clause, I):
   if len(clause) == 0: return True, I
 
   for item in clause:
-    if len(item) == 0: return False
+    if len(item) == 0: return False, None
+
+  L = selectVariable(clause)
+  Lc = "!" + L
+
+  #caso verdadero de la variable
+  Bc = cp.deepcopy(clause)
+
+  for item in Bc:
+    if L in item:
+      Bc.remove(item)
+    elif Lc in item:
+      item.remove(Lc)
+  
+  Ic = cp.deepcopy(I)
+  Ic.append({L:True})
+
+  result, I1 = DPLL(Bc, Ic)
+  if result: return True, I1
+
+  #caso falso de la variable
+  Bc2 = cp.deepcopy(clause)
+
+  for item in Bc2:
+    if Lc in item:
+      Bc2.remove(item)
+    elif L in item:
+      item.remove(L)
+  
+  Ic2 = cp.deepcopy(I)
+  Ic2.append({L:False})
+
+  result2, I2 = DPLL(Bc2, Ic2)
+  if result2: return True, I2
+  return False, None
+
   
 
 
 
 
-#[{"p"}, {"!p"}]
-#[{"q", "p", "!p"}]
-#[{"!p", "!r", "!s"}, {"!q", "!p", "!s"}]
-#[{"!p", "!q"}, {"q", "!s"}, {"!p", "s"}, {"!q", "s"}]
-#[{"!p", "!q", "!r"}, {"q", "!r", "p"}, {"!p", "q", "r"}]
-#[{"r"}, {"!q", "!r"}, {"!p", "q", "!r"}, {"q"}]
+clauses = [
+  [{"p"}, {"!p"}],
+  [{"q", "p", "!p"}],
+  [{"!p", "!r", "!s"}, {"!q", "!p", "!s"}],
+  [{"!p", "!q"}, {"q", "!s"}, {"!p", "s"}, {"!q", "s"}],
+  [{"!p", "!q", "!r"}, {"q", "!r", "p"}, {"!p", "q", "r"}],
+  [{"r"}, {"!q", "!r"}, {"!p", "q", "!r"}, {"q"}]
+]
 
-result, combination = bruteForce([{"r"}, {"!q", "!r"}, {"!p", "q", "!r"}, {"q"}])
-if result:
-  print("Exito con combinación: ", combination)
-else:
-  print("No tiene solucion")
+print("Por fuerza bruta:")
+for i in range(len(clauses)):
+  item = clauses[i]
+  result, combination = bruteForce(item)
+  if result:
+    print("Clausula %i: Exito con combinación: "%(i+1), combination)
+  else:
+    print("Clausula %i: No tiene solucion"%(i+1))
+
+print("\nUtilizando DPLL:")
+for i in range(len(clauses)):
+  item = clauses[i]
+  result, combination = DPLL(item, [])
+  if result:
+    print("Clausula %i: Exito con combinación: "%(i+1), combination)
+  else:
+    print("Clausula %i: No tiene solucion"%(i+1))
